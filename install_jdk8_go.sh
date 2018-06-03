a48c15ec80c49326d4a7dc694c2636d534902756
stiverick610 <stiverick610@gmail.com>
2018-06-03 01:26:54 +0000

    fix bug

--- install_jdk8_go.sh
+++ install_jdk8_go.sh
@@ -1,3 +1,8 @@
+function install_golang()
+{
+sudo apt install -y golang-go
+}
+
 function install_jdk8()
 {
 sudo add-apt-repository -y ppa:webupd8team/java
@@ -5,4 +10,5 @@ sudo apt-get update
 sudo apt-get install -y oracle-java8-installer
 }
 
+install_golang
 install_jdk8
